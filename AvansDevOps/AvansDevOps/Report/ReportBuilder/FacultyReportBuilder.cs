ffcd7cb2b88feff19dac67516a5bae69436b83ca
Tom Schoonbeek <34373221+TJFSchoo@users.noreply.github.com>
2022-06-23 02:57:55 +0200

    SonarCloud fix duplicate code

--- AvansDevOps/AvansDevOps/Report/ReportBuilder/FacultyReportBuilder.cs
+++ AvansDevOps/AvansDevOps/Report/ReportBuilder/FacultyReportBuilder.cs
@@ -7,44 +7,53 @@ using AvansDevOps.Sprint;
 
 namespace AvansDevOps.Report.ReportBuilder
 {
-    public class InternalReportBuilder: IReportBuilder
+    public class FacultyReportBuilder: IReportBuilder
     {
         private readonly ReportModel _report;
 
-        public InternalReportBuilder()
-        {
-            this._report = new ReportModel();
-        }
+        public FacultyReportBuilder()
+        { this._report = new ReportModel(); }
 
-        public void BuildFooter()
+        public void BuildFooter(
+            )
         {
             this._report.Footer = new Footer()
                 { 
                     FooterColor = "#00AA11", 
+
                     FooterName = "Faculty", 
+
                     FooterWebsite = "https://avansfictief.nl/werknemer/" 
                 };
         }
 
-        public void BuildHeader(ISprint sprint, string reportVersion, DateTime date)
+        public void BuildHeader(
+            ISprint sprint, 
+            string reportVersion, 
+            DateTime date)
         {
             this._report.Header = new Header()
             {
                 HeaderColor = "#00AA11",
+
                 HeaderName = "Faculty",
+
                 HeaderLogoURL = "https://avansfictief.nl/werknemer/logo.png",
+
                 Date = date,
+
                 ReportVersion = reportVersion,
+
                 SprintName = sprint.GetName()
             };
         }
 
-        public void BuildContent(List<string> contents)
-        {
-            this._report.Contents = contents;
-        }
+        public void BuildContent(
+            List<string> contents)
+        { this._report.Contents = contents; }
 
-        public ReportModel GetReport(EReportFormat format)
+        public ReportModel GetReport(
+            EReportFormat format)
         {
             this._report.Format = format;
             return this._report;
